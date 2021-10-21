d20ed0a461598cc7be000d8fc4db07331d982b6c
ravshankhx <ravshankhxapple@gmail.com>
2021-10-21 15:58:56 +0300

    changed online to tutorial

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,7 +13,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Hello, PDP ONLINE"),
+        child: Text("Hello, PDP Tutorial"),
       ),
     );
   }
